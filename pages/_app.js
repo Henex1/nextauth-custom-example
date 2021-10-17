f363d9d0361ca401b5b139e35462ffc6b9a96a51
Henry Nnabuchi <68671353+Henex1@users.noreply.github.com>
2021-10-17 02:33:15 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import { Provider } from "next-auth/client";
 import { ChakraProvider } from "@chakra-ui/react";
-
+//
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
